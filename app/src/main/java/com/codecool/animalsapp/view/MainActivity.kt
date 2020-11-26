e37f30488dbdfa7cf678710a55ead10f08aada2e
Drosztmér István <drosztmer.istvan@gmail.com>
2020-11-26 13:38:02 +0100

    Add fragments

--- app/src/main/java/com/codecool/animalsapp/view/MainActivity.kt
+++ app/src/main/java/com/codecool/animalsapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.codecool.animalsapp
+package com.codecool.animalsapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codecool.animalsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
