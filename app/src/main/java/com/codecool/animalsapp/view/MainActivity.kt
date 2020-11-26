b001edb11ba4c8ef52b830a2261c6236ad4d4aa8
Drosztmér István <drosztmer.istvan@gmail.com>
2020-11-26 15:12:46 +0100

    Add navigate up functionality

--- app/src/main/java/com/codecool/animalsapp/view/MainActivity.kt
+++ app/src/main/java/com/codecool/animalsapp/view/MainActivity.kt
@@ -2,11 +2,20 @@ package com.codecool.animalsapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.codecool.animalsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
