756359e0dcaaa9b875614c1706cc3fe2ff54c686
Drosztmér István <drosztmer.istvan@gmail.com>
2020-11-27 12:39:38 +0100

    Add viewmodel with mock data

---
+++ app/src/main/java/com/codecool/animalsapp/viewmodel/ListViewModel.kt
@@ -0,0 +1,33 @@
+package com.codecool.animalsapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.codecool.animalsapp.model.Animal
+
+class ListViewModel(application: Application): AndroidViewModel(application) {
+
+    val animals by lazy { MutableLiveData<List<Animal>>() }
+    val loadError by lazy { MutableLiveData<Boolean>() }
+    val loading by lazy { MutableLiveData<Boolean>() }
+
+    fun refresh () {
+        getAnimals()
+    }
+
+    private fun getAnimals() {
+        val a1 = Animal("Alligator")
+        val a2 = Animal("Bee")
+        val a3 = Animal("Cat")
+        val a4 = Animal("Dog")
+        val a5 = Animal("Elephant")
+        val a6 = Animal("Flamingo")
+
+        val animalList = arrayListOf(a1, a2, a3, a4, a5 ,a6)
+
+        animals.value = animalList
+        loadError.value = false
+        loading.value = false
+    }
+
+}
