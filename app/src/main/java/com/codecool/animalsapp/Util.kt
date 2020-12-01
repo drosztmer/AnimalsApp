45274edc2fa9feea143c89321cddf042d5ff9446
Drosztmér István <drosztmer.istvan@gmail.com>
2020-12-01 15:16:28 +0100

    Load images with glide

---
+++ app/src/main/java/com/codecool/animalsapp/Util.kt
@@ -0,0 +1,27 @@
+package com.codecool.animalsapp
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.codecool.animalsapp.util.CENTER_RADIUS
+import com.codecool.animalsapp.util.STROKE_WIDTH
+
+fun getProgressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = STROKE_WIDTH
+        centerRadius = CENTER_RADIUS
+        start()
+    }
+}
+
+fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.drawable.launcher)
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(uri)
+        .into(this)
+}
--- app/src/main/java/com/codecool/animalsapp/util/Constants.kt
+++ app/src/main/java/com/codecool/animalsapp/util/Constants.kt
@@ -1,3 +1,5 @@
 package com.codecool.animalsapp.util
 
 const val BASE_URL: String = "https://us-central1-apis-4674e.cloudfunctions.net"
+const val STROKE_WIDTH = 10f
+const val CENTER_RADIUS = 50f
