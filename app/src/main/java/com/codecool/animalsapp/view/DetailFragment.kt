be9a623f0168c48e68800395d3d7afc6d7c2975a
Drosztmér István <drosztmer.istvan@gmail.com>
2020-12-02 14:53:49 +0100

    Apply databinding on detailfragment

--- app/src/main/java/com/codecool/animalsapp/view/DetailFragment.kt
+++ app/src/main/java/com/codecool/animalsapp/view/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.palette.graphics.Palette
@@ -13,39 +14,41 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import com.codecool.animalsapp.R
-import com.codecool.animalsapp.model.Animal
+import com.codecool.animalsapp.databinding.FragmentDetailBinding
 import com.codecool.animalsapp.util.getProgressDrawable
 import com.codecool.animalsapp.util.loadImage
-import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
     private val args by navArgs<DetailFragmentArgs>()
 
+    private lateinit var dataBinding: FragmentDetailBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        dataBinding.animal = args.animal
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        populateViewsWithData()
-        args.animal.imageUrl?.let { setupBackgroundColor(it) }
+        dataBinding.animal?.imageUrl?.let { setupBackgroundColor(it) }
     }
 
     private fun setupBackgroundColor(url: String) {
         Glide.with(this)
             .asBitmap()
             .load(url)
-            .into(object: CustomTarget<Bitmap>() {
+            .into(object : CustomTarget<Bitmap>() {
                 override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
                     Palette.from(resource)
                         .generate() { palette ->
                             val intColor = palette?.lightMutedSwatch?.rgb ?: 0
-                            animal_layout.setBackgroundColor(intColor)
+                            dataBinding.animalLayout.setBackgroundColor(intColor)
                         }
                 }
 
@@ -53,14 +56,6 @@ class DetailFragment : Fragment() {
                 }
 
             })
-    }
-
-    private fun populateViewsWithData() {
-        animal_image.loadImage(args.animal.imageUrl, getProgressDrawable(requireContext()))
-        animal_name.text = args.animal.name
-        animal_location.text = args.animal.location
-        animal_lifespan.text = args.animal.lifeSpan
-        animal_diet.text = args.animal.diet
     }
 
 }
