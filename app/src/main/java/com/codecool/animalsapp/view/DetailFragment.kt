286c546c7f685a02cc79d0587fee7f5362d4acd6
Drosztmér István <drosztmer.istvan@gmail.com>
2020-12-02 13:34:29 +0100

    Use palette for background color

--- app/src/main/java/com/codecool/animalsapp/view/DetailFragment.kt
+++ app/src/main/java/com/codecool/animalsapp/view/DetailFragment.kt
@@ -1,11 +1,17 @@
 package com.codecool.animalsapp.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.codecool.animalsapp.R
 import com.codecool.animalsapp.model.Animal
 import com.codecool.animalsapp.util.getProgressDrawable
@@ -26,6 +32,30 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        populateViewsWithData()
+        args.animal.imageUrl?.let { setupBackgroundColor(it) }
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate() { palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+                            animal_layout.setBackgroundColor(intColor)
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+            })
+    }
+
+    private fun populateViewsWithData() {
         animal_image.loadImage(args.animal.imageUrl, getProgressDrawable(requireContext()))
         animal_name.text = args.animal.name
         animal_location.text = args.animal.location
