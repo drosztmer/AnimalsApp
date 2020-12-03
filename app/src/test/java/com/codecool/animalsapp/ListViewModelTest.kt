32cf5a7539fef5f154df346ce87921ddf461a098
Drosztmér István <drosztmer.istvan@gmail.com>
2020-12-03 15:31:44 +0100

    Test api success path

--- app/src/test/java/com/codecool/animalsapp/ListViewModelTest.kt
+++ app/src/test/java/com/codecool/animalsapp/ListViewModelTest.kt
@@ -4,15 +4,19 @@ import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.codecool.animalsapp.di.AppModule
 import com.codecool.animalsapp.di.DaggerViewModelComponent
+import com.codecool.animalsapp.model.Animal
 import com.codecool.animalsapp.model.AnimalApiService
 import com.codecool.animalsapp.util.SharedPreferencesHelper
 import com.codecool.animalsapp.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
@@ -33,6 +37,8 @@ class ListViewModelTest {
 
     var listViewModel = ListViewModel(application, true)
 
+    private val key = "Test key"
+
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
@@ -42,6 +48,21 @@ class ListViewModelTest {
             .prefsModule(PrefsModuleTest(prefs))
             .build()
             .inject(listViewModel)
+    }
+
+    @Test
+    fun getAnimalsSuccess() {
+        Mockito.`when`(prefs.getApiKey()).thenReturn(key)
+        val animal = Animal("cow", null, null, null, null, null, null)
+        val animalList = listOf(animal)
+        val testSingle = Single.just(animalList)
+        Mockito.`when`(animalService.getAnimals(key)).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(1, listViewModel.animals.value?.size)
+        Assert.assertEquals(false, listViewModel.loadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
     }
 
     @Before
